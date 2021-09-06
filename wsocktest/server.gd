e0ed96ab735c29474b4ac33ce127334db99ee3bf
Jairo <howoca2039@invql.com>
2021-09-05 22:16:26 -0300

    finished decoding messages correctly

--- wsocktest/server.gd
+++ wsocktest/server.gd
@@ -20,6 +20,13 @@ func _process(_delta):
 	if _server.is_listening():
 		_server.poll()
 
+func _connected(id, _protocol):
+	print("Client connected!")
+	
+	var peer = _server.get_peer(id)
+	peer.set_write_mode(WebSocketPeer.WRITE_MODE_TEXT)
+	peers[id] = peer
+
 func _disconnected(id, was_clean_close):
 	print("Client disconnected! was_clean_close: ", was_clean_close)
 	if id in peers:
@@ -33,22 +40,26 @@ func _close_request(id, code, reason):
 	])
 
 func _data_received(id):
-	var pct = JSON.parse(peers[id].get_packet().get_string_from_utf8())
-	if pct.error == OK:
-		var msg = pct.result
-		if "payload" in msg and !msg.payload.empty():
-			var payload = JSON.parse(msg.payload)
-			if payload.error == OK:
-				get_node("..").message(msg.type, payload.result)
+	var data = peers[id].get_packet().get_string_from_utf8() as String
+	if data.left(1) in ["[", "{"]:
+		var json = JSON.parse(data.strip_edges().strip_escapes()) as JSONParseResult
+		if typeof(json.result) == TYPE_DICTIONARY:
+			if "payload" in json.result and !json.result.payload.empty():
+				if json.result.payload.left(1) in ["[", "{"]:
+					var payload = JSON.parse(json.result.payload.strip_edges().strip_escapes()) as JSONParseResult
+					json.result.payload = payload.result
+				else:
+					if !json.result.payload.strip_edges().strip_escapes().empty():
+						var payload = []
+						for line in json.result.payload.split("\n"):
+							if !line.empty():
+								if line.right(line.length() - 1) != "=":
+									line += "="
+								payload.push_back(Marshalls.base64_to_raw(line.trim_prefix("b64-")))
+			
+			get_node("..").message(json.result)
 	else:
-		printerr(peers[id].get_packet().get_string_from_utf8())
-
-func _connected(id, _protocol):
-	print("Client connected!")
-	
-	var peer = _server.get_peer(id)
-	peer.set_write_mode(WebSocketPeer.WRITE_MODE_TEXT)
-	peers[id] = peer
+		print("unsupported data: ", data)
 
 func send(msg):
 	if peers.size() == 0:
