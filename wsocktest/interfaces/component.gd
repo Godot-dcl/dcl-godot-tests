f8510acb0296938e7c2f658deb7aed1130b98578
Jeronimo Schreyer <jeronimo.schreyer@gmail.com>
2021-11-11 22:09:16 -0300

    added support for multiple meshes in the component (glb)

--- wsocktest/interfaces/component.gd
+++ wsocktest/interfaces/component.gd
@@ -10,7 +10,7 @@ extends Reference
 #}
 
 var name : String
-var mesh = MeshInstance.new()
+var mesh = [] #MeshInstance.new()
 var mesh_collider : MeshInstance
 var collider : CollisionShape
 var material : SpatialMaterial
@@ -21,13 +21,15 @@ func _init(_name):
 	name = _name
 	match name:
 		"shape":
-			mesh.mesh = CubeMesh.new()
-			mesh.mesh.size = Vector3.ONE
-			mesh.name = name
+			var m = MeshInstance.new()
+			m.mesh = CubeMesh.new()
+			m.mesh.size = Vector3.ONE
+			m.name = name
 
 			material = SpatialMaterial.new()
 			#material.flags_unshaded = true
-			mesh.set("material/0", material)
+			m.set("material/0", material)
+			mesh.push_back(m)
 
 		"material":
 			material = SpatialMaterial.new()
@@ -36,6 +38,7 @@ func _init(_name):
 func update(data):
 	var json = JSON.parse(data).result
 	if json.has("src"):
+		mesh.clear()
 		var ext = json.src.get_extension()
 		if ext == "glb":
 			var content = ContentManager.get_instance(json.src)
@@ -45,7 +48,7 @@ func update(data):
 					if child.name.ends_with("_collider"):
 						mesh_collider = child
 					else:
-						mesh = child
+						mesh.push_back(child)
 
 	if json.has("withCollisions"):
 		var static_body: StaticBody
@@ -53,10 +56,11 @@ func update(data):
 			mesh_collider.create_trimesh_collision()
 			static_body = mesh_collider.get_child(0)
 			mesh_collider.remove_child(static_body)
-			mesh.add_child(static_body)
+			mesh[0].add_child(static_body)
 		else:
-			mesh.create_trimesh_collision()
-			static_body = mesh.get_child(0)
+			var m = mesh[0]
+			m.create_trimesh_collision()
+			static_body = m.get_child(0)
 
 		if json.has("isPointerBlocker"):
 			static_body.collision_layer = int(pow(2, 10) + pow(2, 11) + pow(2, 12))
@@ -83,14 +87,15 @@ func update(data):
 		material.params_use_alpha_scissor = true
 		material.params_alpha_scissor_threshold = json.alphaTest
 
-	if json.has("castShadows"):
-		mesh.cast_shadow = json.castShadows
-
-	if json.has("visible"):
-		mesh.visible = json.visible
+#	if json.has("castShadows"):
+#		mesh.cast_shadow = json.castShadows
+#
+#	if json.has("visible"):
+#		mesh.visible = json.visible
 
 func attach_to(entity):
 	if name == "material":
 		entity.get_node("shape").set("material/0", material)
 	else:
-		entity.add_child(mesh.duplicate())
+		for m in mesh:
+			entity.add_child(m.duplicate())
