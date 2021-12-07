5e2759cba0f62a5c8547cd45787ebc2b2c7f5323
Lisandro Lorea <lisandrolorea@gmail.com>
2021-12-07 19:34:02 -0300

    Implement VideoClip and VideoTexture

---
+++ wsocktest/interfaces/components/video_clip.gd
@@ -0,0 +1,23 @@
+extends "res://interfaces/component.gd"
+class_name DCL_VideoClip
+
+const _classid = 70
+
+var url : String
+
+var video_clip : VideoStream
+
+var error = true
+
+func _init(_name).(_name):
+	pass
+
+func update(data):
+	var json = JSON.parse(data).result
+	if json.has("url"):
+		url = json.url
+		if url.begins_with("http"):
+			push_error("Error loading VideoClip. Real time streaming not supported by Godot")
+		else:
+			video_clip = ContentManager.get_instance(url)
+			error = false
