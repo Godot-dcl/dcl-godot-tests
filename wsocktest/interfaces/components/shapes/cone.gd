59c939d654848bc8acdc741fa0bc5ed50a48ff22
Lisandro Lorea <lisandrolorea@gmail.com>
2021-12-09 18:39:54 -0300

    Forgot to add the new init parameters to cone.gd and cylinder.gd

--- wsocktest/interfaces/components/shapes/cone.gd
+++ wsocktest/interfaces/components/shapes/cone.gd
@@ -4,7 +4,7 @@ class_name DCL_ConeShape
 const _classid = 19
 
 
-func _init(_name).(_name):
+func _init(_name, _scene, _id).(_name, _scene, _id):
 	var cone = CylinderMesh.new()
 	cone.height = 1.0
 	cone.top_radius = 0.0
