cc2f52a2a958f3e54138281b9a98717b5da85f18
Lisandro Lorea <lisandrolorea@gmail.com>
2021-12-10 11:28:46 -0300

    Support downloading external sources
    Since Godot doesn't support streaming video in real time it needs to be downloaded completely before playback.
    In that case the VideoTexture will return a texture showing the text "Buffering.." until the video is downloaded.
    Once the download is finished if playing is set to true it'll start playing the video

--- wsocktest/interfaces/components/video_clip.gd
+++ wsocktest/interfaces/components/video_clip.gd
@@ -3,11 +3,14 @@ class_name DCL_VideoClip
 
 const _classid = 70
 
+const SUPPORTED_EXTENSIONS = ["ogg", "ogv"]
+
 var url : String
 
 var video_clip : VideoStream
 
-var error = true
+enum { STATUS_READY, STATUS_ERRORED, STATUS_PREPARING }
+var status = STATUS_ERRORED
 
 func _init(_name, _scene, _id).(_name, _scene, _id):
 	pass
@@ -16,8 +19,57 @@ func update(data):
 	var json = JSON.parse(data).result
 	if json.has("url"):
 		url = json.url
-		if url.begins_with("http"):
-			push_error("Error loading VideoClip. Real time streaming not supported by Godot")
-		else:
-			video_clip = ContentManager.get_instance(url)
-			error = false
+		status = STATUS_ERRORED
+		var ext = url.get_extension()
+		var is_external = url.begins_with("http")
+		var is_https = url.begins_with("https")
+
+		if not ext in SUPPORTED_EXTENSIONS:
+			status = STATUS_ERRORED
+			push_error("DCL_VideoClip: Unsupported extention: %s" % ext)
+			return
+
+		if not is_https:
+			status = STATUS_ERRORED
+			push_error("DCL_VideoClip: http is unsupported. Please use https" % ext)
+			return
+
+		if is_external:
+			status = STATUS_PREPARING
+			var download = _get_external_video(url)
+			if download is GDScriptFunctionState:
+				yield(download,"completed")
+			if not url == json.url: #If it doesn't match a new url was set and we no longer want the file
+				return
+
+		video_clip = ContentManager.get_instance(url)
+		status = STATUS_READY
+
+func _get_external_video(url: String):
+	var content = {"file": url, "hash" : str(url.hash())}
+	if ContentManager.file_downloaded(content):
+		ContentManager.cache_file(content)
+		return
+	
+	var http = HTTPRequest.new()
+	http.use_threads = false
+	scene.add_child(http)
+	var res = http.request(url)
+	var response = yield(http,"request_completed")
+	if res != OK:
+		printerr("****** error creating the request: ", res)
+		return
+
+	response.append(content)
+	callv("download_file", response)
+	http.queue_free()
+
+
+func download_file(_result, response_code, _headers, body, content):
+	if response_code >= 200 and response_code < 300:
+		var f = File.new()
+		var file_name = "user://%s.%s" % [content.hash, content.file.get_extension()]
+		if f.open(file_name, File.WRITE) == OK:
+			f.store_buffer(body)
+			f.close()
+	ContentManager.cache_file(content)
--- wsocktest/interfaces/components/video_texture.gd
+++ wsocktest/interfaces/components/video_texture.gd
@@ -1,4 +1,4 @@
-extends "res://interfaces/component.gd"
+extends "res://interfaces/component.gd"  #TODO: Extend DCL_Texture when implemented
 class_name DCL_VideoTexture
 
 const _classid = 71
@@ -11,26 +11,42 @@ var playing : float = false
 var videoClipId : String
 
 var video_player : VideoPlayer
+
+var viewports = [] # to queue free when deleting the component
+
 var texture : Texture
+signal texture_changed(new_texture)
 
 func _init(_name, _scene, _id).(_name, _scene, _id):
 	video_player = VideoPlayer.new()
 	video_player.connect("finished", self, "_on_finished")
 	video_player.hide()
 	video_player.name = name + str(get_instance_id())
-
+	
 	scene.add_child(video_player)
 
 func update(data):
 	var json = JSON.parse(data).result
 	if json.has("videoClipId"):
 		videoClipId = json.videoClipId
-		var clip = scene.components[videoClipId]
-		if clip.error:
-			texture = _create_error_texture("Could not load: \n" + clip.url, scene)
-		else:
+		var clip : DCL_VideoClip = scene.components[videoClipId]
+
+		if clip.status == DCL_VideoClip.STATUS_PREPARING:
+			texture = _create_error_texture("Buffering...", scene, viewports)
+			emit_signal("texture_changed", texture)
+
+		while clip.status == DCL_VideoClip.STATUS_PREPARING:
+			yield(scene.get_tree(),"idle_frame")
+		
+		if clip.status == DCL_VideoClip.STATUS_ERRORED:
+			texture = _create_error_texture("Could not load: \n" + clip.url, scene, viewports)
+			emit_signal("texture_changed", texture)
+			
+		elif clip.status == DCL_VideoClip.STATUS_READY and videoClipId == json.videoClipId:
 			video_player.stream = clip.video_clip
 			texture = video_player.get_video_texture()
+			emit_signal("texture_changed", texture)
+
 
 	if json.has("playing"):
 		playing = json.playing
@@ -57,21 +73,22 @@ func _on_finished():
 	if loop:
 		video_player.play()
 
-static func _create_error_texture(text : String, scene) -> Texture:
+static func _create_error_texture(text : String, scene, viewports : Array ) -> Texture:
 	var ret : Texture
 	var vport_name = "error_texture" + str(text.hash())
-	if Server.get_tree().current_scene.has_node(vport_name):
-		ret = Server.get_tree().current_scene.get_node(vport_name).get_texture()
+	if scene.has_node(vport_name):
+		ret = scene.get_node(vport_name).get_texture()
 	else:
 		var font = load("res://fonts/inter/default_text.tres")
 		
 		var vport = Viewport.new()
 		vport.name = vport_name
 		vport.size = Vector2(640,480)
-		vport.render_target_update_mode = Viewport.UPDATE_ALWAYS
+		vport.render_target_update_mode = Viewport.UPDATE_ONCE
 		vport.render_target_v_flip = true
 		
 		scene.add_child(vport)
+		viewports.append(vport)
 		
 		var label = Label.new()
 		vport.add_child(label)
