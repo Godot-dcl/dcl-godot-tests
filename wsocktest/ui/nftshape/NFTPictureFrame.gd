7575d409a9defeb623f850e26dbcad32e76b82ac
Lisandro Lorea <lisandrolorea@gmail.com>
2022-01-27 00:41:41 -0300

    Implement Classic, Blocky, Golden_Carved and Golden_Wide Picture Frames
    TODO: the fbx files can be found at unity-renderer\Assets\Scripts\MainScripts\DCL\Components\LoadableShapes\NFTShape\Meshes
    I converted them to gltf using an online converter and had to fix some
    textures by hand.
    
    The materials don't look completely correct. I think the unity version
    is using materials set/modified in Unity.
    
    Someone more familiar with 3D asset export/import should do a proper
    conversion to gltf so they are easier to implement

--- wsocktest/ui/nftshape/NFTPictureFrame.gd
+++ wsocktest/ui/nftshape/NFTPictureFrame.gd
@@ -32,20 +32,58 @@ var material : SpatialMaterial
 var color : Color
 
 func _init():
-	mesh = CubeMesh.new()
-	translate_object_local( Vector3(0,0,0.051) )
+	mesh = ArrayMesh.new()
+	translate_object_local( Vector3(0,0,0.031) )
 
 func set_style(style : int, size : Vector2):
 	_style = style
 	match style:
 		#Implement all the different styles
+		
+		PictureFrameStyle.Classic:
+			var model = preload("res://3d/NFTShape/Classic.gltf").instance()
+			var new_mesh = model.get_child(0).mesh.duplicate(true)
+			mesh = new_mesh
+			mesh.surface_get_material(0).albedo_color = color
+			mesh.surface_get_material(0).emission = color
+			mesh.surface_get_material(1).albedo_color = color
+			mesh.surface_get_material(1).emission = color
+			mesh.surface_get_material(2).emission_energy = 10
+		
+		PictureFrameStyle.Blocky:
+			var model = preload("res://3d/NFTShape/Blocky_01.gltf").instance()
+			var new_mesh = model.get_child(0).mesh.duplicate(true)
+			mesh = new_mesh
+			mesh.surface_get_material(0).albedo_color = color
+			mesh.surface_get_material(0).emission_enabled = false
+			mesh.surface_get_material(1).albedo_color = color
+			mesh.surface_get_material(1).emission_enabled = false
+		
+		PictureFrameStyle.Gold_Carved:
+			var model = preload("res://3d/NFTShape/Golden_01.gltf").instance()
+			var new_mesh = model.get_child(0).mesh.duplicate(true)
+			mesh = new_mesh
+			mesh.surface_get_material(0).emission_enabled = false
+			mesh.surface_get_material(1).albedo_color = color
+			mesh.surface_get_material(1).emission = color
+			mesh.surface_get_material(2).albedo_color = color
+			mesh.surface_get_material(2).emission = color
+		
+		PictureFrameStyle.Gold_Wide:
+			var model = preload("res://3d/NFTShape/Golden_03.gltf").instance()
+			var new_mesh = model.get_child(0).mesh.duplicate(true)
+			mesh = new_mesh
+			mesh.surface_get_material(0).emission_enabled = false
+			mesh.surface_get_material(1).albedo_color = color
+			mesh.surface_get_material(1).emission = color
+			mesh.surface_get_material(2).albedo_color = color
+			mesh.surface_get_material(2).emission = color
 		_:
-			mesh.size = Vector3( size.x,size.y,0.1 )
-			material = SpatialMaterial.new()
-			mesh.surface_set_material(0, material)
-			material.flags_unshaded = true
-			update_color(color)
+			printerr("Unimplemented PictureStyle %s" % PictureFrameStyle.keys()[style])
+	scale = Vector3(size.x, size.y, 1.0)
+	
 
 func update_color(c : Color):
 	material.albedo_color = c
 	material.emission = c
+	
