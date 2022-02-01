d09b6463a050cd3f031e09e78ba8c625aab2ab60
Lisandro Lorea <lisandrolorea@gmail.com>
2022-01-31 23:01:42 -0300

    Add cone shape creation support to godot4

--- wsocktest/scene/scene.gd
+++ wsocktest/scene/scene.gd
@@ -76,6 +76,9 @@ func message(scene_msg):
 
 			DCL_PlaneShape._classid:
 				components[c_id] = DCL_PlaneShape.new(c_name, self, c_id)
+				
+			DCL_ConeShape._classid:
+				components[c_id] = DCL_ConeShape.new(c_name, self, c_id)
 
 			DCL_Material._classid:
 				components[c_id] = DCL_Material.new(c_name, self, c_id)
