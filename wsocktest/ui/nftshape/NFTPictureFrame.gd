2a70aa55e2528cf477804a7c97ebc16fdda4cbb7
Lisandro Lorea <lisandrolorea@gmail.com>
2022-02-10 03:32:22 -0300

    Implement all PictureFrames

--- wsocktest/ui/nftshape/NFTPictureFrame.gd
+++ wsocktest/ui/nftshape/NFTPictureFrame.gd
@@ -25,11 +25,39 @@ enum PictureFrameStyle {
 	Wood_Twigs, #20
 	Canvas,
 	None
- }
+	}
+
+var meshes : Dictionary = {
+	PictureFrameStyle.Classic : preload("res://3d/NFTShape/Classic.gltf"),
+	PictureFrameStyle.Baroque_Ornament : preload("res://3d/NFTShape/Barroque_01.gltf"),
+	PictureFrameStyle.Diamond_Ornament : preload("res://3d/NFTShape/Barroque_02.gltf"),
+	PictureFrameStyle.Minimal_Wide : preload("res://3d/NFTShape/Basic_01.gltf"),
+	PictureFrameStyle.Minimal_Grey : preload("res://3d/NFTShape/Basic_02.gltf"),
+	PictureFrameStyle.Blocky : preload("res://3d/NFTShape/Blocky_01.gltf"),
+	PictureFrameStyle.Gold_Edges : preload("res://3d/NFTShape/Golden_01.gltf"),
+	PictureFrameStyle.Gold_Carved : preload("res://3d/NFTShape/Golden_02.gltf"),
+	PictureFrameStyle.Gold_Wide : preload("res://3d/NFTShape/Golden_03.gltf"),
+	PictureFrameStyle.Gold_Rounded : preload("res://3d/NFTShape/Golden_04.gltf"),
+	PictureFrameStyle.Metal_Medium : preload("res://3d/NFTShape/Metal_01.gltf"),
+	PictureFrameStyle.Metal_Wide : preload("res://3d/NFTShape/Metal_02.gltf"),
+	PictureFrameStyle.Metal_Slim : preload("res://3d/NFTShape/Metal_03.gltf"),
+	PictureFrameStyle.Metal_Rounded : preload("res://3d/NFTShape/Metal_04.gltf"),
+	PictureFrameStyle.Pins : preload("res://3d/NFTShape/Pin.gltf"),
+	PictureFrameStyle.Minimal_Black : preload("res://3d/NFTShape/SimpleBlack.gltf"),
+	PictureFrameStyle.Minimal_White : preload("res://3d/NFTShape/SimpleWhite.gltf"),
+	PictureFrameStyle.Tape : preload("res://3d/NFTShape/Tapper.gltf"),
+	PictureFrameStyle.Wood_Slim : preload("res://3d/NFTShape/Wood.gltf"),
+	PictureFrameStyle.Wood_Wide : preload("res://3d/NFTShape/Wood_02.gltf"),
+	PictureFrameStyle.Wood_Twigs : preload("res://3d/NFTShape/WoodSticks.gltf"),
+	PictureFrameStyle.Canvas: preload("res://3d/NFTShape/SimpleCanvas.gltf"),
+	PictureFrameStyle.None : preload("res://3d/NFTShape/SimpleWhite.gltf") #Anything goes, it just simplifies code
+}
 
 var _style : int = PictureFrameStyle.Classic
 var material : StandardMaterial3D
 var color : Color
+
+enum { BORDER, BACKGROUND, DETAIL } # Not all styles obey this
 
 func _init():
 	mesh = ArrayMesh.new()
@@ -37,49 +65,114 @@ func _init():
 
 func set_style(style : int, size : Vector2):
 	_style = style
+	var model = meshes[style].instantiate()
+	var new_mesh = model.get_child(0).mesh.duplicate(true)
+	mesh = new_mesh
+
 	match style:
-		#Implement all the different styles
-		
 		PictureFrameStyle.Classic:
-			var model = preload("res://3d/NFTShape/Classic.gltf").instantiate()
-			var new_mesh = model.get_child(0).mesh.duplicate(true)
-			mesh = new_mesh
-			mesh.surface_get_material(0).albedo_color = color
-			mesh.surface_get_material(0).emission = color
-			mesh.surface_get_material(1).albedo_color = color
-			mesh.surface_get_material(1).emission = color
-			mesh.surface_get_material(2).emission_energy = 10
-		
+			mesh.surface_get_material(BORDER).albedo_color = color
+			mesh.surface_get_material(BORDER).emission = color
+			mesh.surface_get_material(BACKGROUND).albedo_color = color
+			mesh.surface_get_material(BACKGROUND).emission = color
+			mesh.surface_get_material(DETAIL).emission_energy = 10
+
+		PictureFrameStyle.Canvas:
+			mesh.surface_get_material(BORDER).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).albedo_color = color
+
+		PictureFrameStyle.Baroque_Ornament:
+			mesh.surface_get_material(BORDER).albedo_color = Color.GOLDENROD
+			mesh.surface_get_material(BORDER).emission = Color.GOLDENROD
+			mesh.surface_get_material(BACKGROUND).albedo_color = Color.GOLDENROD
+			mesh.surface_get_material(DETAIL).albedo_color = Color.GOLDENROD
+
+		PictureFrameStyle.Diamond_Ornament:
+			mesh.surface_get_material(BORDER).albedo_color = Color.GOLDENROD
+			mesh.surface_get_material(BORDER).emission = Color.CHOCOLATE
+			mesh.surface_get_material(BACKGROUND).albedo_color = Color.GOLDENROD
+			mesh.surface_get_material(DETAIL).albedo_color = Color.GOLDENROD
+
+		PictureFrameStyle.Minimal_Wide:
+			mesh.surface_get_material(BACKGROUND).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).albedo_color = color
+			mesh.surface_get_material(DETAIL).albedo_color = color
+
+		PictureFrameStyle.Minimal_Grey:
+			mesh.surface_get_material(BORDER).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).emission_enabled = false
+			mesh.surface_get_material(DETAIL).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).albedo_color = color
+
+		PictureFrameStyle.Minimal_Black:
+			mesh.surface_get_material(BORDER).emission_enabled = false
+			mesh.surface_get_material(BORDER).albedo_color = color
+			mesh.surface_get_material(BORDER).shading_mode = StandardMaterial3D.SHADING_MODE_UNSHADED
+			mesh.surface_get_material(BACKGROUND).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).albedo_color = color
+			mesh.surface_get_material(BACKGROUND).shading_mode = StandardMaterial3D.SHADING_MODE_UNSHADED
+			mesh.surface_get_material(DETAIL).emission_enabled = false
+			mesh.surface_get_material(DETAIL).albedo_color = Color.BLACK
+
+		PictureFrameStyle.Minimal_White:
+			mesh.surface_get_material(BORDER).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).albedo_color = color
+			mesh.surface_get_material(DETAIL).emission_enabled = false
+
+		PictureFrameStyle.Wood_Slim, PictureFrameStyle.Wood_Wide:
+			mesh.surface_get_material(BORDER).emission_enabled = false
+			mesh.surface_get_material(BORDER).albedo_texture = preload("res://3d/NFTShape/Floor_Wood01.png")
+			mesh.surface_get_material(BACKGROUND).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).albedo_color = color
+			mesh.surface_get_material(DETAIL).emission_enabled = false
+
+		PictureFrameStyle.Wood_Twigs:
+			mesh.surface_get_material(BORDER).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).albedo_texture = preload("res://3d/NFTShape/Genesis_TX.png")
+
+		PictureFrameStyle.Tape:
+			mesh.surface_get_material(BORDER).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).albedo_color = color
+			mesh.surface_get_material(DETAIL).emission_enabled = false
+
+		PictureFrameStyle.Metal_Medium, PictureFrameStyle.Metal_Wide, PictureFrameStyle.Metal_Slim, PictureFrameStyle.Metal_Rounded:
+			mesh.surface_get_material(BORDER).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).albedo_color = color
+			mesh.surface_get_material(BACKGROUND).emission_enabled = false
+			
+		PictureFrameStyle.Pins:
+			mesh.surface_get_material(BORDER).emission_enabled = false
+			mesh.surface_get_material(BORDER).albedo_texture = preload("res://3d/NFTShape/Genesis_TX.png")
+			mesh.surface_get_material(BACKGROUND).albedo_color = color
+			mesh.surface_get_material(BACKGROUND).shading_mode = StandardMaterial3D.SHADING_MODE_UNSHADED
+			mesh.surface_get_material(BACKGROUND).emission_enabled = false
+			mesh.surface_get_material(DETAIL).albedo_color = color
+			mesh.surface_get_material(DETAIL).emission_enabled = false
+			mesh.surface_get_material(DETAIL).shading_mode = StandardMaterial3D.SHADING_MODE_UNSHADED
+
 		PictureFrameStyle.Blocky:
-			var model = preload("res://3d/NFTShape/Blocky_01.gltf").instantiate()
-			var new_mesh = model.get_child(0).mesh.duplicate(true)
-			mesh = new_mesh
-			mesh.surface_get_material(0).albedo_color = color
-			mesh.surface_get_material(0).emission_enabled = false
-			mesh.surface_get_material(1).albedo_color = color
-			mesh.surface_get_material(1).emission_enabled = false
-		
-		PictureFrameStyle.Gold_Carved:
-			var model = preload("res://3d/NFTShape/Golden_01.gltf").instantiate()
-			var new_mesh = model.get_child(0).mesh.duplicate(true)
-			mesh = new_mesh
-			mesh.surface_get_material(0).emission_enabled = false
-			mesh.surface_get_material(1).albedo_color = color
-			mesh.surface_get_material(1).emission = color
-			mesh.surface_get_material(2).albedo_color = color
-			mesh.surface_get_material(2).emission = color
-		
-		PictureFrameStyle.Gold_Wide:
-			var model = preload("res://3d/NFTShape/Golden_03.gltf").instantiate()
-			var new_mesh = model.get_child(0).mesh.duplicate(true)
-			mesh = new_mesh
-			mesh.surface_get_material(0).emission_enabled = false
-			mesh.surface_get_material(1).albedo_color = color
-			mesh.surface_get_material(1).emission = color
-			mesh.surface_get_material(2).albedo_color = color
-			mesh.surface_get_material(2).emission = color
+			mesh.surface_get_material(BORDER).albedo_color = color
+			mesh.surface_get_material(BORDER).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).albedo_color = color
+			mesh.surface_get_material(BACKGROUND).emission_enabled = false
+
+		PictureFrameStyle.Gold_Carved, PictureFrameStyle.Gold_Edges, PictureFrameStyle.Gold_Wide, PictureFrameStyle.Gold_Rounded:
+			mesh.surface_get_material(BORDER).emission_enabled = false
+			mesh.surface_get_material(BACKGROUND).albedo_color = color
+			mesh.surface_get_material(BACKGROUND).emission = color
+			mesh.surface_get_material(DETAIL).albedo_color = color
+			mesh.surface_get_material(DETAIL).emission = color
+
+		PictureFrameStyle.None:
+			mesh = null
+
 		_:
 			printerr("Unimplemented PictureStyle %s" % PictureFrameStyle.keys()[style])
+
 	scale = Vector3(size.x, size.y, 1.0)
 	
 
